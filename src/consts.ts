6d88fa076faea8b83a62090933a804899c83f334
floodfx <donnie@floodfx.com>
2025-08-14 20:27:20 +0000

    configure site for donnie.com domain
    Co-Authored-By: sketch <hello@sketch.dev>
    Change-ID: sce6d34282b381a03k

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://floodfx.github.io";
-export const SITE_TITLE = "Your Blog";
+export const SITE_URL = "https://donnie.com";
+export const SITE_TITLE = "Donnie";
 export const SITE_DESCRIPTION =
   "A minimal blog built with Astro and Tailwind CSS.";
 
-export const EMAIL = "your-email@example.com";
+export const EMAIL = "hello@donnie.com";
